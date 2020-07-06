a324754e79713b81c06c5a160acff133d21f256a
Adam Turner <adam.turner@wsu.edu>
2020-07-06 10:19:25 -0700

    :sparkles: Add plugin load and lifecycle management functions
    Create the base plugin file, load file, and plugin meta management
    file. Lifecycle functions handle updating plugin meta data such as
    version and status on plugin activation, deactivation, and
    uninstallation.

---
+++ lib/load.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Load the HRSWP Documents plugin.
+ *
+ * @package HRSWP_Documents
+ * @since 1.0.0
+ */
+
+namespace HrswpDocuments\lib\load;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Silence is golden.' );
+}
+
+require dirname( __FILE__ ) . '/data.php';
