f991d5b75ce83bdbbd25e7962f454f05933defdc
Adam Turner <act@adamturner.dev>
2020-10-21 11:42:31 -0700

    :package: Update built files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '98fe2af9193e60b8fbbb9eb9731c4d9b');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '5e8896fa1cd7837aebf1eb573e79761d');
